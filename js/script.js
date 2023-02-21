77477dfe03f14fcb8e657367621ab248bc0f8898
Vlad Tipukhov <vlad1kk728@ukr.net>
2023-02-21 12:36:53 +0200

    checkbox

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
-function fun1(){
-    let checkbox;
-    checkbox = document.getElementById('one');
+const car = document.getElementsByName("car");
+const button = document.querySelector(".button")
+function whichCar(){
+      if(car[0].checked) {
+           alert('Haval made in China');
+        } else if (car[1].checked) {
+        alert('Audi made in German');
+        } else if (car[2].checked) {
+            alert('Lexus made in Japan');
+        }
+    } 
+button.addEventListener("click", whichCar);
 
-    if(checkbox.checked) {
-        alert('Вибраний');
-    } else {
-        alert('Не вибраний');
-    }
-}
+
+
+
+
+
+// function fun1(){
+//     let checkbox;
+//     checkbox = document.getElementById('one');
+
+//     if(checkbox.checked) {
+//         alert('Вибраний');
+//     } else {
+//         alert('Не вибраний');
+//     }
+// }
+// function fun1(){
+//     let radio = document.getElementsByName('r1');
+//     for (i = 0; i < radio.length; i ++){
+//         if(radio[i].checked) {
+//             alert('Вибраний ' + i + ' елемент');
+//         }
+//     }
+// }
