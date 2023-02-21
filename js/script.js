409e292b1cac87aff13b328c1d78e73023d78514
Vlad Tipukhov <vlad1kk728@ukr.net>
2023-02-21 15:02:54 +0200

    radio

--- js/script.js
+++ js/script.js
@@ -1,36 +1,13 @@
 const car = document.getElementsByName("car");
-const button = document.querySelector(".button")
+const buttonRadio = document.getElementById("button_radio")
 function whichCar(){
       if(car[0].checked) {
-           alert('Haval made in China');
+            alert('Haval made in China');
         } else if (car[1].checked) {
-        alert('Audi made in German');
+            alert('Audi made in German');
         } else if (car[2].checked) {
             alert('Lexus made in Japan');
         }
     } 
-button.addEventListener("click", whichCar);
-
-
-
-
-
-
-// function fun1(){
-//     let checkbox;
-//     checkbox = document.getElementById('one');
-
-//     if(checkbox.checked) {
-//         alert('Вибраний');
-//     } else {
-//         alert('Не вибраний');
-//     }
-// }
-// function fun1(){
-//     let radio = document.getElementsByName('r1');
-//     for (i = 0; i < radio.length; i ++){
-//         if(radio[i].checked) {
-//             alert('Вибраний ' + i + ' елемент');
-//         }
-//     }
-// }
+buttonRadio.addEventListener("click", whichCar);
+// =================================================
